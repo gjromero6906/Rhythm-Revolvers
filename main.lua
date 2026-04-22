1e5c2ffc039f7002ad1712f13560f61b3aeec533
gjromero6906 <gjr6906@gmail.com>
2026-04-22 14:02:25 -0400

    Initial commit: LÖVE game setup

---
+++ main.lua
@@ -0,0 +1,29 @@
+-- LÖVE game framework
+-- Documentation: https://love2d.org/wiki/Main_Page
+
+-- Window constants
+local WIDTH = 800
+local HEIGHT = 600
+
+function love.load()
+    -- Set window size
+    love.window.setMode(WIDTH, HEIGHT)
+    love.window.setTitle("LÖVE Game")
+    
+    -- Initialize game
+end
+
+function love.update(dt)
+    -- Update game logic
+end
+
+function love.draw()
+    -- Draw graphics
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
